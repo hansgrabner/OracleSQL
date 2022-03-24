9df117ba4c15ded8f3d09c6b99ebaf6d651df283
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 18:22:47 +0100

    Update PLSQLHelloWorld.sql

--- PLSQLHelloWorld.sql
+++ PLSQLHelloWorld.sql
@@ -1,1 +1,52 @@
 
+--https://www.tutorialspoint.com/plsql/plsql_quick_guide.htm
+
+set serveroutput on
+
+DECLARE 
+   message  varchar2(20):= 'Hello, World!'; 
+BEGIN 
+   dbms_output.put_line(message); 
+END; 
+
+DECLARE 
+   SUBTYPE name IS char(20); 
+   SUBTYPE message IS varchar2(100); 
+   salutation name; 
+   greetings message; 
+BEGIN 
+   salutation := 'Reader '; 
+   greetings := 'Welcome to the World of PL/SQL'; 
+   dbms_output.put_line('Hello ' || salutation || greetings); 
+END; 
+/ 
+
+
+
+DECLARE 
+   a integer := 10; 
+   b integer := 20; 
+   c integer; 
+   f real; 
+BEGIN 
+   c := a + b; 
+   dbms_output.put_line('Value of c: ' || c); 
+   f := 70.0/3.0; 
+   dbms_output.put_line('Value of f: ' || f); 
+END; 
+
+
+
+DECLARE 
+   i number(1); 
+   j number(1); 
+BEGIN 
+   << outer_loop >> 
+   FOR i IN 1..3 LOOP 
+      << inner_loop >> 
+      FOR j IN 1..3 LOOP 
+         dbms_output.put_line('i is: '|| i || ' and j is: ' || j); 
+      END loop inner_loop; 
+   END loop outer_loop; 
+END; 
+/
