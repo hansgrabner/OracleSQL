74b47d2d2791ca930ae464af9f0275eb67226c1e
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 11:30:30 +0100

    Update Mod05Joins.sql

--- Mod05Joins.sql
+++ Mod05Joins.sql
@@ -1,1 +1,7 @@
 
+SELECT First_Name, Department_Name
+FROM  Employees, Departments -- CROSS JOIN oder Kartesisches Produkt
+
+SELECT First_Name, Department_Name
+    FROM  Employees JOIN Departments 
+    ON Employees.department_id = departments.department_id
