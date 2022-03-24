978c6681f74952038f42f1dd193349ecb978e3c0
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 09:42:21 +0100

    Update Kap04AggregatedData.sql

--- Kap04AggregatedData.sql
+++ Kap04AggregatedData.sql
@@ -5,3 +5,16 @@ SELECT 'HELLO Oracle' FROM Dual;
 SELECT First_name, length(last_name), substr(JOB_ID,1,3) AS ErsterDrei,
 'Hello', 12+234, first_name || ' wie gehts?' AS Begruessung
 FROM employees ;
+
+
+
+
+SELECT MAX(salary)
+FROM employees;
+
+SELECT MAX(salary)
+FROM employees
+WHERE department_id=10; --schränkt die Berchnungsgrundlage ein
+
+SELECT MIN(First_name) AS KleinsterVorname, MAX(hire_date) AS LetzteAnstellung
+FROM Employees;
