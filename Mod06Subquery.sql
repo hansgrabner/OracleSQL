3f32b75568a198f029499f71c7c85aebc031227d
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 14:47:27 +0100

    Update Mod06Subquery.sql

--- Mod06Subquery.sql
+++ Mod06Subquery.sql
@@ -18,5 +18,21 @@ SELECT First_name, salary,
 salary - (SELECT ROUND(AVG(salary)) FROM employees) as Differenz
 FROM  Employees;
 
+Name der Abteilung mit den meisten Mitarbeitern
+JOIN Having Subquery
 
+Group By          Count
+ Abteilung A      17
+ Abteilung B      23
+ 
+ 
+ SELECT d.department_name, COUNT(e.employee_id)
+FROM departments d JOIN  Employees e
+ON d.department_id = e.department_id
+GROUP BY d.department_name
+HAVING COUNT(e.employee_id) = 45
+ORDER BY COUNT(e.employee_id) DESC
+ 
+ 
+ 
 
