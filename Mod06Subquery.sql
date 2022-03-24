4ffe1703807b4e77a811def87926f1b9f4f559cc
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 14:29:03 +0100

    Update Mod06Subquery.sql

--- Mod06Subquery.sql
+++ Mod06Subquery.sql
@@ -1,1 +1,14 @@
 
+1 Aufgabe "Geringverdiener"
+
+                
+SELECT MIN(salary) FROM employees;
+
+SELECT First_name, Last_name
+FROM employees 
+WHERE salary = (SELECT MIN(salary) FROM employees);
+
+
+SELECT First_name, Last_name
+FROM employees 
+WHERE salary <= ALL (SELECT salary FROM employees);
