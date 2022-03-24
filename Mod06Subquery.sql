dec434407af31f88efb8d3243df66f058f28d915
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 14:34:30 +0100

    Update Mod06Subquery.sql

--- Mod06Subquery.sql
+++ Mod06Subquery.sql
@@ -12,3 +12,11 @@ WHERE salary = (SELECT MIN(salary) FROM employees);
 SELECT First_name, Last_name
 FROM employees 
 WHERE salary <= ALL (SELECT salary FROM employees);
+
+SELECT First_name, salary, 
+(SELECT ROUND(AVG(salary)) FROM employees) As Durchschnitt,
+salary - (SELECT ROUND(AVG(salary)) FROM employees) as Differenz
+FROM  Employees;
+
+
+
