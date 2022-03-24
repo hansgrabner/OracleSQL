3b54f2ca4564e218722871fc0c706604743305a2
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 11:49:16 +0100

    Update Mod05Joins.sql

--- Mod05Joins.sql
+++ Mod05Joins.sql
@@ -2,6 +2,33 @@
 SELECT First_Name, Department_Name
 FROM  Employees, Departments -- CROSS JOIN oder Kartesisches Produkt
 
-SELECT First_Name, Department_Name
-    FROM  Employees JOIN Departments 
+SELECT First_Name, Department_Name, departments.department_Id
+    FROM  Employees JOIN Departments --INNER JOIN EQUI
     ON Employees.department_id = departments.department_id
+    
+  SELECT emp.First_Name, frizi.Department_Name,frizi.department_Id
+    FROM  Employees emp JOIN Departments frizi--INNER JOIN EQUI
+    ON emp.department_id = frizi.department_id
+    
+    
+      
+    
+SELECT First_Name, Department_Name
+    FROM  Employees RIGHT OUTER JOIN Departments 
+    ON Employees.department_id = departments.department_id
+    where employee_id is null
+    
+SELECT First_Name AS F, Department_Name
+    FROM  Departments LEFT OUTER JOIN Employees
+    ON Employees.department_id = departments.department_id
+    
+    ORDER BY F
+    
+    SELECT First_Name, Department_Name
+    FROM  Departments FULL OUTER JOIN Employees
+    ON Employees.department_id = departments.department_id
+    
+    
+    SELECT First_Name, Department_Name
+    FROM  Employees NATURAL JOIN Departments 
+   
