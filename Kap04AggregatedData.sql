c1830a08bda78effa38914eb02fff427c01c7eeb
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-24 09:16:27 +0100

    Update Kap04AggregatedData.sql

--- Kap04AggregatedData.sql
+++ Kap04AggregatedData.sql
@@ -1,2 +1,7 @@
 --my first hello world
 SELECT 'HELLO Oracle' FROM Dual;
+
+
+SELECT First_name, length(last_name), substr(JOB_ID,1,3) AS ErsterDrei,
+'Hello', 12+234, first_name || ' wie gehts?' AS Begruessung
+FROM employees ;
