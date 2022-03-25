33cc29218128108e58e48d314f02a3645e44c448
hansgrabner <35722210+hansgrabner@users.noreply.github.com>
2022-03-25 09:33:42 +0100

    Update WiederholungFreitag.sql

--- WiederholungFreitag.sql
+++ WiederholungFreitag.sql
@@ -1,1 +1,24 @@
 
+
+
+SELECT 'Hello ' || ' + ' || ' World ' FROM DUAL;
+
+
+SELECT * from departments;
+
+
+
+SELECT e.First_Name, d.department_name, 'mit join'
+FROM Employees e JOIN Departments d
+ON e.department_id = d.department_id
+WHERE d.department_name='Marketing'
+
+UNION
+
+SELECT e.First_Name, '', 'Mit Subquery'
+FROM Employees e
+WHERE e.department_id = (
+        SELECT department_id 
+        FROM departments 
+        WHERE department_name='Marketing'
+)
